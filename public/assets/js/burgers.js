39053454299e96f8f2188e24218c98dc5a36c547
Rita Manley <manley.rita@gmail.com>
2019-09-28 15:01:02 -0700

    add burger button adds with no devoured value

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,43 +1,36 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-    $(function() {
-//     $(".submit").on("click", function(event) {
-//         var id = $(this).data("id");
-//         var newDevour = $(this).data("newdevour");
+$(function() {
+    $(".change-eat").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
 
-//         var newDevourState = {
-//             devoured: newDevour
-//         };
-//         console.log(newDevourState);
+        var newDevourState = {
+            devoured: newDevour
+        };
 
-//         // Send the PUT request
-//         $.ajax("/api/burgers/" + id, {
-//             type: "PUT",
-//             data: newDevourState
-//         }).then(
-//             function() {
-//                 alert("Mmmmm. That IS a TASTY burger!");
-//                 // Reload the page to get the updated list
-//                 location.reload();
-//             }
-//         );
-//     });
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                alert("Yummy!");
+                location.reload();
+            }
+        );
+    });
 
-    $(".submit").on("click", function(event) {
+    $(".create-form").on("submit", function(event) {
         event.preventDefault();
         
         var newBurger = {
-            burger_name: $("#burger_name").val().trim(),
-            devoured: 0
+            burger_name: $("#burger_name").val().trim()
         };
-        // console.log(newBurger);
         // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(
             function() {
-                alert("New burger--begging to be Devoured!");
-                // Reload the page to get the updated list
+                alert("Here's your burger!");
                 location.reload();
             }
         );
@@ -51,8 +44,7 @@
             type: "DELETE"
         }).then(
             function() {
-                alert("Burger has been 86'ed from the Menu!", id);
-                // Reload the page to get the updated list
+                alert("It's outta here!", id);
                 location.reload();
             }
         );
--- server.js
+++ server.js
@@ -5,21 +5,28 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// parse application/json
 app.use(bodyParser.json());
 
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var router = require("./controllers/burgers_controller.js");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-app.use(router);
+app.use(routes);
 
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });

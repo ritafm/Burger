e1d107e39054d7bd60d09a7da294566197a2019a
Rita Manley <manley.rita@gmail.com>
2019-09-27 21:18:41 -0700

    styling okay. functionality nowhere.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+// app.use("../models/burger", burger)
+
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+            res.render("index", {burgers:data});
+            console.table(data)
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        res.json(result);
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('tavern burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', false);

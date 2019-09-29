0e7cc5b90b45d1825c8210222e68bfe6196d9233
Rita Manley <manley.rita@gmail.com>
2019-09-28 18:59:45 -0700

    still with the heroku

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
 var mysql = require('mysql');
+var connection = require("dotenv").config();
 
-var connection;
-
-if(process.env.JAWSDB_URL){
+if (process.env.NODE_ENV === "production") {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
     connection = mysql.createConnection({

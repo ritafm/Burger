d8ccf356611c5ffc7f5940d5736be50e23654463
Rita Manley <manley.rita@gmail.com>
2019-09-28 19:58:07 -0700

    HEROKUUUUU

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

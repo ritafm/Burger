0013f8f03996e576f8e1e00d648f40f6d66e43fa
Rita Manley <manley.rita@gmail.com>
2019-09-30 15:36:32 -0700

    changed connection.js to reflect jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 var connection = require("dotenv").config();
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
     connection = mysql.createConnection({
         host: 'localhost',
-        PORT: 8080,
+        PORT: 3306,
         user: 'root',
         password: "password",
         database: "burgers_db"

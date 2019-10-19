aabed51adae4263b3f566994668f31ac44f3a332
Rita Manley <manley.rita@gmail.com>
2019-10-19 10:00:55 -0700

    cleaned up commented out code

--- config/connection.js
+++ config/connection.js
@@ -16,38 +16,5 @@ if(process.env.JAWSDB_URL) {
   });
 } 
 
-//var mysql = require('mysql');
-// var connection = require('dotenv').config();
-
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL)
-// } else {
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'password',
-//         database: 'skimqhpshyalrotz'
-//     });
-
-    // if (process.env.NODE_ENV === 'production') {
-    //     connection = mysql.createConnection(process.env.JAWSDB_URL);
-    // }else {
-    //     connection = mysql.createConnection({
-    //         host: 'localhost',
-    //         user: 'root',
-    //         password: 'password',
-    //         database: 'skimqhpshyalrotz'
-    //     });
-    // };
-
-
-    // connection.connect(function(err)  {
-    //     if(err) {
-    //         console.error("error connecting: " + err.stack);
-    //         return;
-    //     }
-    //     console.log("connected as id " + connection.threadId);
-    // });
-
     connection.connect();
     module.exports = connection;

046f7f90fe776adbac61aecbddd753b68f32a416
Rita Manley <manley.rita@gmail.com>
2019-10-19 09:38:52 -0700

    changed connection.js to match tassa

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,33 @@
+//if we are on Heroku and the JawsDB addon is configured, we will have the environment variable JAWSDB_URL
+//this variable will contain what mysql package needs to connect :)
+require('dotenv').config();
 var mysql = require('mysql');
-var connection = require('dotenv').config();
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
+var connection;
+if(process.env.JAWSDB_URL) {  
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'skimqhpshyalrotz'
-    });
+  //otherwise, we're going to use our local connection!  put your local db set stuff here
+  //(and remember our best practice of using the dotenv package and a .env file ;)
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.LOCAL_DB_USER,
+    password: process.env.LOCAL_DB_PASSWORD,
+    database: process.env.LOCAL_DB_DATABASE
+  });
+} 
+
+//var mysql = require('mysql');
+// var connection = require('dotenv').config();
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'password',
+//         database: 'skimqhpshyalrotz'
+//     });
 
     // if (process.env.NODE_ENV === 'production') {
     //     connection = mysql.createConnection(process.env.JAWSDB_URL);
